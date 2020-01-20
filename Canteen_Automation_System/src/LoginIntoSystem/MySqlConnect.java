100b211864af03150b58535ed800f4272b36b830
Rohankumar Maisuria <rmaisuria@horizon.csueastbay.edu>
2020-01-20 09:16:53 -0800

    Canteen automation system

---
+++ Canteen_Automation_System/src/LoginIntoSystem/MySqlConnect.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LoginIntoSystem;
+import java.sql.*;
+import javax.swing.*;
+/**
+ *
+ * @author Rohan
+ */
+public class MySqlConnect {
+    Connection conn=null;
+    public static Connection ConnectDB(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost/canteen","root","05121989");
+            JOptionPane.showMessageDialog(null,"Connected to database");
+            return conn;
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+            return null;
+        }
+    }
+}
